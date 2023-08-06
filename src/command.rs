78c58cf0b293f1d5229b7a9cb3b9fd84a2da88de
Alexandro Cu <alexandro.cuma@gmail.com>
2023-08-06 17:40:23 -0600

    fix(cli): Fixed Command gen Options regarding commit type

--- src/command.rs
+++ src/command.rs
@@ -4,6 +4,12 @@ pub fn cli(args: &GenArgs) -> std::string::String {
   let type_message = match args.commit_type {
     CommitTypes { fix: true, .. } => "fix".to_owned(),
     CommitTypes { feat: true, .. } => "feat".to_owned(),
+    CommitTypes { docs: true, .. } => "docs".to_owned(),
+    CommitTypes { style: true, .. } => "style".to_owned(),
+    CommitTypes { refactor: true, .. } => "refactor".to_owned(),
+    CommitTypes { perf: true, .. } => "perf".to_owned(),
+    CommitTypes { test: true, .. } => "test".to_owned(),
+    CommitTypes { chore: true, .. } => "chore".to_owned(),
     _ => "".to_owned()
   };
   let scope = if args.scope.is_some() { format!("({})", args.scope.as_ref().unwrap().to_owned()) } else { "".to_owned() };
