b1c9acdae94537617d67dbd8f310607b6e75bc40
deleonn <joel.dl08@gmail.com>
2023-08-12 00:52:36 -0600

    fix: format project using 'cargo fmt' command

--- src/cli_struct.rs
+++ src/cli_struct.rs
@@ -3,62 +3,62 @@ use clap::{Args, Parser, Subcommand};
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Cli {
-  #[command(subcommand)]
-  pub command: Option<Commands>,
+    #[command(subcommand)]
+    pub command: Option<Commands>,
 }
 
 #[derive(Subcommand, Debug)]
 pub enum Commands {
-  Init,
-  Gen(GenArgs)
+    Init,
+    Gen(GenArgs),
 }
 
 #[derive(Debug, Args)]
 pub struct GenArgs {
-  #[command(flatten)]
-  pub commit_type: CommitTypes,
+    #[command(flatten)]
+    pub commit_type: CommitTypes,
 
-  #[arg(short, long)]
-  pub scope: Option<String>,
+    #[arg(short, long)]
+    pub scope: Option<String>,
 
-  #[arg(short = 'b', long)]
-  pub breaking_change: bool,
+    #[arg(short = 'b', long)]
+    pub breaking_change: bool,
 
-  #[arg(short, long, required = true)]
-  pub description: Option<String>,
+    #[arg(short, long, required = true)]
+    pub description: Option<String>,
 
-  #[arg(short = 'o', long)]
-  pub body: Option<String>,
+    #[arg(short = 'o', long)]
+    pub body: Option<String>,
 
-  #[arg(short = 't', long)]
-  pub footer: Option<String>,
+    #[arg(short = 't', long)]
+    pub footer: Option<String>,
 }
 
 #[derive(Args, Debug)]
 #[group(required = true, multiple = false)]
 
 pub struct CommitTypes {
-  #[arg(long)]
-  pub fix: bool,
+    #[arg(long)]
+    pub fix: bool,
 
-  #[arg(long)]
-  pub feat: bool,
+    #[arg(long)]
+    pub feat: bool,
 
-  #[arg(long)]
-  pub docs: bool,
+    #[arg(long)]
+    pub docs: bool,
 
-  #[arg(long)]
-  pub style: bool, 
+    #[arg(long)]
+    pub style: bool,
 
-  #[arg(long)]
-  pub refactor: bool,
+    #[arg(long)]
+    pub refactor: bool,
 
-  #[arg(long)] 
-  pub perf: bool,   
+    #[arg(long)]
+    pub perf: bool,
 
-  #[arg(long)]
-  pub test: bool,
+    #[arg(long)]
+    pub test: bool,
 
-  #[arg(long)]
-  pub chore: bool,  
+    #[arg(long)]
+    pub chore: bool,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,39 @@
-
 use clap::Parser;
 use console::Style;
 
+mod cli_struct;
+mod command;
 mod configuration;
 mod git_actions;
 mod interactive;
-mod cli_struct;
-mod command;
 mod shared;
 
-use git_actions::commit;
 use crate::configuration::create_configuration;
+use git_actions::commit;
 
 fn main() {
-  let cyan = Style::new().cyan();
-  let cli = cli_struct::Cli::parse();
+    let cyan = Style::new().cyan();
+    let cli = cli_struct::Cli::parse();
 
-  match &cli.command {
-    Some(cli_struct::Commands::Init) => {
-      create_configuration(".convmit/");
+    match &cli.command {
+        Some(cli_struct::Commands::Init) => {
+            create_configuration(".convmit/");
+        }
+        Some(cli_struct::Commands::Gen(args)) => {
+            let conventional_commit = command::cli(args);
+            commit::commit(&conventional_commit);
+            println!(
+                "Changes commited with the message:\n\n{}\n",
+                cyan.apply_to(&conventional_commit)
+            );
+        }
+        None => {
+            let conventional_commit = interactive::cli();
+            commit::commit(&conventional_commit);
+            println!(
+                "Changes commited with the message:\n\n{}\n",
+                cyan.apply_to(&conventional_commit)
+            );
+        }
     }
-    Some(cli_struct::Commands::Gen(args)) => {
-      let conventional_commit = command::cli(args);
-      commit::commit(&conventional_commit);
-      println!("Changes commited with the message:\n\n{}\n", cyan.apply_to(&conventional_commit));
-    }
-    None => {
-      let conventional_commit = interactive::cli();
-      commit::commit(&conventional_commit);
-      println!("Changes commited with the message:\n\n{}\n", cyan.apply_to(&conventional_commit));
-    }
-  }
 }
